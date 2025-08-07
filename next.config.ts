86cff76f25637e9e9a753ec2ef4fa80e7982b995
Praneeth Gunasekara <praneeth01@gmail.com>
2025-08-07 15:17:49 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -18,9 +18,8 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    allowedDevOrigins: ['https://*.cloudworkstations.dev'],
-  },
+  experimental: {},
+  allowedDevOrigins: ['https://*.cloudworkstations.dev'],
 };
 
 export default nextConfig;
